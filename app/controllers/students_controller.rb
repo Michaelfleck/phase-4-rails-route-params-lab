acd2f5dec1eb20efcd4422e3b0d17858529d753b
mfleck <michael.j.fleck@gmail.com>
2021-12-16 16:05:38 -0600

    commit tree ac8395d1473e2fca152b82e08f3042307952408a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,41 @@
 class StudentsController < ApplicationController
 
   def index
+    name = params[:name]
     students = Student.all
-    render json: students
+    student = Student.where("first_name=? COLLATE NOCASE OR last_name=? COLLATE NOCASE", name, name)
+    
+    if student.length == 0
+      render json: students
+    else
+      render json: student
+    end
+  end
+
+  def find_student
+    find_student = Student.find(params[:id])
+    render json: find_student
+  end
+
+
+  # Order.where("created_at > #{params[:start_date]} 
+  # AND user_id = #{current_user.id}")
+
+  # #---------------------------------------------------------------
+
+  # Order.where("created_at > ? AND user_id = ?", 
+  # params[:start_date], current_user.id)
+
+  # #results in
+  # SELECT users.* FROM users WHERE 
+  # (created_at > '2018-12-21 OR 1 --' AND user_id = 1234)
+
+  def find_student_byname
+    qstringdata = request.query_string
+    
+    # name= params[:name]
+    # sbn = Student.where("first_name=? OR last_name=?", name, name)
+    # render json: sbn
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   get '/students', to: 'students#index'
+  get '/students/testing', to: 'students#find_student_byname'
+
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+  
+  get '/students/:id', to: 'students#find_student'
+  # get '/students/:name', to: 'students#find_student_byname'
+
+  # get '/students?name=:name', to: 'students#find_student_byname'
+
+  # '/students?name=kirby'
 end
